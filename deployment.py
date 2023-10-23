62e0684faa8b03c05a36170ef170721a1038ec90
“AFA <ashesifootball.contractarchive@gmail.com>
2023-10-23 18:19:11 +0000

    .

--- deployment.py
+++ deployment.py
@@ -1,41 +1,37 @@
-import streamlit as st
+# Import necessary libraries
 import pickle
+import streamlit as st
 import pandas as pd
+import numpy as np
 
 # Load the trained RandomForestRegressor model
 model = pickle.load(open('player_rating_predictor.pkl', 'rb'))
 
-# Create a function to make predictions
-def predict_player_rating(input_data):
-    return model.predict(input_data)
-
-# Streamlit UI
+# Create a Streamlit web app
 st.title("FIFA Player Rating Predictor")
 
-# Create input fields for user to enter player attributes
-st.sidebar.header("Enter Player Attributes")
+# Create input elements for user to provide player profile data
+st.header("Enter Player Profile Data")
+age = st.slider("Age", 16, 45, 25)
+overall = st.slider("Overall Rating", 50, 100, 75)
+potential = st.slider("Potential Rating", 50, 100, 80)
 
-# Define input fields for relevant features (modify as needed)
-features = ['movement_reactions', 'mentality_composure', 'passing', 'potential', 'release_clause_eur', 'dribbling', 'wage_eur', 'power_shot_power', 'value_eur', 'mentality_vision', 'attacking_short_passing']
-input_data = []
+# Create a button to predict the rating
+if st.button("Predict Rating"):
+    # Prepare input data for prediction
+    input_data = np.array([age, overall, potential]).reshape(1, -1)
+    
+    # Make a prediction
+    prediction = model.predict(input_data)
+    
+    # Display the predicted rating
+    st.subheader("Predicted Player Rating:")
+    st.write(prediction[0])
 
-for feature in features:
-    input_value = st.sidebar.number_input(f"Enter {feature}", value=0)
-    input_data.append(input_value)
+# Add an explanation about the model and how to use the app
+st.write("This web app uses a RandomForestRegressor model to predict a player's rating based on their profile data.")
+st.write("You can adjust the player's age, overall rating, and potential rating using the sliders above, and click the 'Predict Rating' button to see the predicted rating.")
 
-# Create a button to make predictions
-if st.sidebar.button("Predict"):
-    input_data = [input_data]
-    prediction = predict_player_rating(input_data)
-    st.write(f"Predicted Player Rating: {prediction[0]:.2f}")
-
-
-
-# Deploy the Streamlit app
-if __name__ == "__main__":
-    st.set_page_config(page_title="FIFA Player Rating Predictor", page_icon="⚽")
-    st.sidebar.markdown("By Your Name")
-    st.write("Welcome to the FIFA Player Rating Predictor!")
-    st.write("Enter player attributes on the left sidebar to predict the player's rating.")
-
+# Optionally, display model confidence score if available
+st.write("Confidence scores can be displayed if provided by the model.")
 
